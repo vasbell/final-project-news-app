c9504f34fc3578d7d8f647f330913f76bf0a1eaf
Vassilis Bellos <vasilisbell@gmail.com>
2024-06-23 17:29:55 +0300

    screens creation

---
+++ GlobalNews/src/navigation/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AppNavigation() {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+    </View>
+  )
+}
