6ee2839e3f0f81e4b082cda43046abfe8397d457
Vassilis Bellos <vasilisbell@gmail.com>
2024-06-23 18:24:50 +0300

    coding

--- GlobalNews/src/navigation/index.js
+++ GlobalNews/src/navigation/index.js
@@ -1,10 +1,89 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useColorScheme } from 'react-native'
+import HomeScreen from "../screens/HomeScreen"
+import DiscoverScreen from "../screens/DiscoverScreen"
+import SavedScreen from "../screens/SavedScreen"
+import SearchScreen from "../screens/SearchScreen"
+import SplashScreens from "../screens/SplashScreens"
+import WelcomeScreen from "../screens/WelcomeScreen"
+import NewsDetailsScreen from "../screens/NewsDetailsScreen"
+import { Ionicons } from '@expo/vector-icons'
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
+  const TabNavigator = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={( {route} ) => ({
+          headerShown: false,
+          tabBarIcon: ({focused}) => {
+            let iconName;
+
+            if(route.name === 'Home') {
+              iconName = 'home';
+            } else if(route.name === 'Discover') {
+              iconName = 'compass-outline'
+            } else if(route.name === 'Saved') {
+              iconName = 'bookmark-outline'
+            } else if(route.name === 'Search') {
+              iconName = 'search'
+            }
+
+            const customizeSize = 25
+            return (
+              <Ionicons
+              name={iconName}
+              size={customizeSize}
+              color={focused ? 'green' : 'gray'}
+              />
+            )
+          },
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: 'gray',
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontFamily: "RobotoBold",
+          },
+          tabBarStyle: {
+            backgroundColor: colorScheme === 'dark' ? 'black' : 'white',
+          }
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Discover" component={DiscoverScreen} />
+        <Tab.Screen name="Saved" component={SavedScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+      </Tab.Navigator>
+    );
+  };
+  
+  
   return (
-    <View>
-      <Text>AppNavigation</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='SplashS'
+        screenOptions={{
+          headerShown:true
+        }}>
+        
+        <Stack.Screen name="SplashS" component={SplashScreens} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen
+          name="Newsdetails"
+          component={NewsDetailsScreen}
+          options={{animation: "slide_from_bottom"}}
+        />
+
+        <Stack.Screen name="HomeTabs" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- GlobalNews/src/screens/SplashScreens.js
+++ GlobalNews/src/screens/SplashScreens.js
@@ -1,10 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback, useEffect } from 'react';
+import { View, Text, ImageBackground } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function SplashScreens() {
-  return (
-    <View>
-      <Text>SplashScreens</Text>
-    </View>
-  )
-}
+    const navigation = useNavigation();
+
+    const [fontsLoaded] = useFonts({
+        Roboto: require("../fonts/Roboto-Regular.ttf"),
+        RobotoBold: require('../fonts/Roboto-Bold.ttf'),
+        RobotoBlack: require('../fonts/Roboto-Black.ttf'),
+        RobotoLight: require('../fonts/Roboto-Light.ttf'),  
+    
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+          setTimeout(() => {
+            navigation.navigate("Welcome");
+          }, 4000);
+        }
+      }, [fontsLoaded]);
+
+    useEffect(() => {
+        onLayoutRootView();
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+
+    return (
+        <ImageBackground
+            source={require("../../assets/images/reporter.jpg")}
+            className="flex1 justify-center items-center"
+        >
+            <LinearGradient
+            colors= {["rgba(0,85,0,0.95)", "rgba(0,85,0,0.95)"]}
+            style={{
+                position: "absolute",
+                bottom: 0,
+                width: "100%",
+                height: "100%",
+            }}
+            
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+            />
+            <View onLayout={onLayoutRootView}>
+                <Text className="text-white text-3xl. font-extrabold uppercase">
+                    Stack Newss
+                </Text>
+            </View>
+        </ImageBackground>
+    );
+};
